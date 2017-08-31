f8f0976740dc6b4ce95ff06bbb31cd636cceb476
Kira Krasnova <vaokyn@gmail.com>
2017-08-31 22:41:40 +0300

    Изменение функции parse

--- common_func.py
+++ common_func.py
@@ -1,32 +1,73 @@
 class Except(BaseException):
 	def __init__(self, arg):
 		self.mess = arg
+	def __repr__(self):
+		return self.mess
+	def __str__(self):
+		return self.mess
 
-def parse(req_str,bdict): 
+# def parse(req_str,bdict): 
+# 	try:
+# 		req_str=req_str.split()
+# 		if len(req_str)<len(bdict): 
+# 			raise Except("Проверьте количество елементов запроса.")
+# 		elif len(req_str)==len(bdict):
+# 			return dict(zip(bdict,req_str))
+# 		else:
+# 			resdict=bdict.copy()
+# 			key=list(resdict.keys())
+# 			rkey=key.copy()
+# 			rkey.reverse()
+# 			for obj in key:
+# 				if resdict[obj]:
+# 					resdict.update({obj:"".join(req_str.pop(0))})
+# 				else:
+# 					break
+# 			for obj in rkey:
+# 				if resdict[obj]:
+# 					resdict.update({obj:"".join(req_str.pop(len(req_str)-1))})
+# 				else:
+# 					resdict.update({obj:"_".join(req_str)})
+# 					break
+# 			return resdict
+# 	except Except as i:
+# 		raise i
+# 	except Exception as e:
+# 		raise e
+
+def parse(p_str, base_dict, sep="_"):
 	try:
-		req_str=req_str.split()
-		if len(req_str)<len(bdict): 
-			raise Except("Проверьте количество елементов запроса.")
-		elif len(req_str)==len(bdict):
-			return dict(zip(bdict,req_str))
-		else:
-			resdict=bdict.copy()
-			key=list(resdict.keys())
-			rkey=key.copy()
-			rkey.reverse()
+		pars_list=p_str.split()
+		if len(pars_list)<len(base_dict):
+			raise Excep("Неполный запрос")
+		if len(pars_list)==len(base_dict):
+			return dict(zip(base_dict,pars_list))
+		if len(pars_list)==sum(base_dict.values()):
+			for i in base_dict:
+				up=[pars_list.pop(0) for i in range(base_dict[i])]
+				base_dict.update({i:sep.join(up)})
+			return base_dict
+		if 0 in base_dict.values():
+			key=list(base_dict.keys())
+			key.reverse()
+			for obj in base_dict:
+				if base_dict[obj]:
+					up=[pars_list.pop(0) for _ in range(base_dict[obj])]
+					base_dict.update({obj:sep.join(up)})
+				else:
+					break
 			for obj in key:
-				if resdict[obj]:
-					resdict.update({obj:"".join(req_str.pop(0))})
+				if base_dict[obj]:
+					up=[pars_list.pop() for _ in range(base_dict[obj])]
+					base_dict.update({obj:sep.join(up)})
 				else:
+					base_dict.update({obj:sep.join(pars_list)})
 					break
-			for obj in rkey:
-				if resdict[obj]:
-					resdict.update({obj:"".join(req_str.pop(len(req_str)-1))})
-				else:
-					resdict.update({obj:"_".join(req_str)})
-					break
-			return resdict
+			return base_dict
+		else:
+			raise Excep("Дерьмо случается")
 	except Except as i:
 		raise i
 	except Exception as e:
 		raise e
+
