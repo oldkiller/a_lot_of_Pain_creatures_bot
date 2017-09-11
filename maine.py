4810821dedca42dd1384b0f5f637489f48d01fb5
Kira Krasnova <vaokyn@gmail.com>
2017-09-11 22:21:24 +0300

    Проверка замены парсинга ручного на парсинг регулярками.V12

--- maine.py
+++ maine.py
@@ -66,7 +66,7 @@ def forecast(message):
 	print(req_key)
 	res=[i for i in data["list"] if i["dt_txt"][11:13] in tlist[::req_key]]
 	#print(txt.num(), "\n", (txt.num()[0] * (8/req_key)) )
-	bot.send_message(message.chat.id, "suka")
+	bot.send_message(message.chat.id, type(txt.num()))
 	if txt.num():
 		for i in res[:txt.num()[0] * (8/req_key)]:
 			bot.send_message(message.chat.id, i["dt_txt"]+" : "+weath_reply(i))
