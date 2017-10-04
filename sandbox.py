b3e5772e923eed675bb13c6e2667928389b5a9aa
Kira Krasnova <vaokyn@gmail.com>
2017-10-04 23:10:44 +0300

    I plan to get rid of regex, and a little clean the repository

--- sandbox.py
+++ sandbox.py
@@ -10,37 +10,90 @@ bot = telebot.TeleBot("317266897:AAGxt_oKV19_LG_S-xbcTH26eb8ZDGD06Fs")
 # REQS=r"^([^0-9])([A-Za-z\-0-9]){1,}$"
 #Ключ: 1 символ, запрос: все,что больше 1 символа, числа: все только числовые значения
 
+# class PWR():
+# 	def __init__(self,txt):
+# 		txt=txt.split()[1:]
+# 		# bd=dict(key=r"^[[:alpha:]]{,2}$", req=r"^([^\d])([\w\-]){2,}$", num=r"^([^[:alpha:]])([\d])$")
+# 		# bd=dict(key=r"^[[:alpha:]]{,2}$", req=r"^(^[\d])([\w\-]){2,}$", num=r"^(^[\w\-])([\d]+)$")
+# 		# bd=dict(key=r"^[[:alpha:]]{1,2}$", req=r"^([^0-9])([\w\-]){2,}$", num=r"^([^A-Za-z])([0-9]){0,}$")
+# 		bd=dict(key=r"^([^\d])([\w]){1}$", req=r"^([^\d])([\w\-]){2,}$", num=r"^([^A-Za-z])([0-9]){0,}$")
+# 		rd={i:[] for i in bd}
+# 		for i in bd:
+# 			for j in txt:
+# 				if re.search(bd[i],j):
+# 					rd[i].append(j)
+# 		self.rd=rd
+	
+# 	def __repr__(self):
+# 		return str(self.rd)
+	
+# 	def __str__(self):
+# 		return str(self.rd)
+
+# 	def __bool__(self):
+# 		return bool(self.rd["key"] or self.rd["req"] or self.rd["num"])
+
+# 	def key(self):
+# 		return self.rd["key"]
+	
+# 	def req(self):
+# 		return self.rd["req"]
+	
+# 	def num(self):
+# 		return [int(i) for i in self.rd["num"]]
+
 class PWR():
-	def __init__(self,txt):
-		txt=txt.split()[1:]
-		# bd=dict(key=r"^[[:alpha:]]{,2}$", req=r"^([^\d])([\w\-]){2,}$", num=r"^([^[:alpha:]])([\d])$")
-		# bd=dict(key=r"^[[:alpha:]]{,2}$", req=r"^(^[\d])([\w\-]){2,}$", num=r"^(^[\w\-])([\d]+)$")
-		# bd=dict(key=r"^[[:alpha:]]{1,2}$", req=r"^([^0-9])([\w\-]){2,}$", num=r"^([^A-Za-z])([0-9]){0,}$")
+	def __init__(self,txtb,sep="$"):
+
+		# bd=dict(key=r"^[a-z]{1,2}$", req=r"^([^0-9])([A-Za-z\-0-9]){2,}$", num=r"^([^A-Za-z])([0-9]){0,}$")
 		bd=dict(key=r"^([^\d])([\w]){1}$", req=r"^([^\d])([\w\-]){2,}$", num=r"^([^A-Za-z])([0-9]){0,}$")
 		rd={i:[] for i in bd}
+		if sep in txtb[1:]:
+			txt=txtb.split(sep)[0]
+			txt=txt.split()[1:]
+			rd["req"].append(txtb[1:].split(sep)[1])
+		else:
+			txt=txtb.split()[1:]
 		for i in bd:
 			for j in txt:
 				if re.search(bd[i],j):
 					rd[i].append(j)
 		self.rd=rd
-	
+
 	def __repr__(self):
 		return str(self.rd)
-	
+
 	def __str__(self):
 		return str(self.rd)
 
 	def __bool__(self):
 		return bool(self.rd["key"] or self.rd["req"] or self.rd["num"])
 
-	def key(self):
-		return self.rd["key"]
+	def key(self,default=None):
+		return self.rd["key"] if self.rd["key"] else [default]
+
+	def req(self,default=None):
+		return self.rd["req"] if self.rd["req"] else [default]
+
+	def num(self,default=None):
+		return [int(i) for i in self.rd["num"]] if self.rd["num"] else [default]
 	
-	def req(self):
-		return self.rd["req"]
-	
-	def num(self):
-		return [int(i) for i in self.rd["num"]]
+	def fkey(self,default=None):
+		return self.rd["key"][0] if self.rd["key"] else default
+
+	def freq(self,default=None):
+		return self.rd["req"][0] if self.rd["req"] else default
+
+	def fnum(self,default=None):
+		return int(self.rd["num"][0]) if self.rd["num"] else default
+
+@bot.message_handler(commands=["test"])
+def test(message):
+	pm=PWR(message.text,sep="/")
+	bot.send_message(message.chat.id, str(pm))
+	# lang="-".join(pm.key())
+	# text=" ".join(pm.req())
+	# bot.send_message(message.chat.id, text+" ||| "+lang)
 
 @bot.message_handler(commands=["rex"])
 def rex(message):
